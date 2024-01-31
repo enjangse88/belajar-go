32520196cbec133ba8e7540d1382e9dbd7a9a45a
enjangse88 <enjang.setiawan88@gmail.com>
2024-01-31 10:16:01 +0700

    add function

---
+++ function/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+// 	sentence := printMyFunction("Saya sedang ")
+// 	fmt.Println(sentence)
+result := Add(10, 5)
+fmt.Println(result)
+
+
+}
+
+// func printMyFunction(sentence string) string {
+// 	newSentence := sentence + "belajar golang"
+// 	return newSentence
+// }
+
+func Add(number, numberTwo int) int {
+	result := number + numberTwo
+	return result
+}
